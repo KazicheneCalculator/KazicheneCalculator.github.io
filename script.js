816085dae30baf60d088cd11ecdb61f90f1c3dbd
Edward <97116266+Anzkiii@users.noreply.github.com>
2023-04-16 17:06:06 +0300

    lotsa changes

--- script.js
+++ script.js
@@ -24,8 +24,37 @@ function calculate() {
     result += combination * fifth_multiplier;
     result += combination * sixth_multiplier;
     result *= 1.2;
-    result /= 1000
+    
 
+    console.log(combination);
+    document.getElementById("result").innerHTML = result.toFixed(2) + "лева."
+}
+
+function calculate_no_multipliers(){
+    var t1_new = parseFloat(document.getElementById("t1_new").value);
+    var t1_old = parseFloat(document.getElementById("t1_old").value);
+    var t2_new = parseFloat(document.getElementById("t2_new").value);
+    var t2_old = parseFloat(document.getElementById("t2_old").value);
+    var first_multiplier = 0.11146;
+    var second_multiplier = 0.02629;
+    var third_multiplier = 0.00648;
+    var fourth_multiplier = 0.00068;
+    var fifth_multiplier = 0.06759;
+    var sixth_multiplier = 0.01989;
+    var day_energy = t1_new - t1_old;
+    var night_energy = t2_new - t2_old;
+    var combination = day_energy + night_energy;
+    
+    var result = 0;
+
+    result += day_energy * first_multiplier;
+    result += night_energy * second_multiplier;
+    result += combination * third_multiplier;
+    result += combination * fourth_multiplier;
+    result += combination * fifth_multiplier;
+    result += combination * sixth_multiplier;
+    result *= 1.2;
+    
     console.log(combination);
     document.getElementById("result").innerHTML = result.toFixed(2) + "лева."
 }
