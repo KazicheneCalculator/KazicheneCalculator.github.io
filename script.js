dbc09b0d3798db89a18d8694033e22507f0f214b
Edward <130863756+KazicheneCalculator@users.noreply.github.com>
2023-04-16 17:52:54 +0300

    changeeeeeeeeeeeeees

--- script.js
+++ script.js
@@ -58,3 +58,36 @@ function calculate_no_multipliers(){
     console.log(combination);
     document.getElementById("result").innerHTML = result.toFixed(2) + "лева."
 }
+
+function calculate_baraka() {
+    var t1_new = parseFloat(document.getElementById("t1_new").value);
+    var t1_old = parseFloat(document.getElementById("t1_old").value);
+    var t2_new = parseFloat(document.getElementById("t2_new").value);
+    var t2_old = parseFloat(document.getElementById("t2_old").value);
+    var first_multiplier = parseFloat(document.getElementById("1st_multiplier").value);
+    var second_multiplier = parseFloat(document.getElementById("2nd_multiplier").value);
+    var third_multiplier = parseFloat(document.getElementById("3rd_multiplier").value);
+    var fourth_multiplier = parseFloat(document.getElementById("4th_multiplier").value);
+    var fifth_multiplier = parseFloat(document.getElementById("5th_multiplier").value);
+    var sixth_multiplier = parseFloat(document.getElementById("6th_multiplier").value);
+    var seventh_multiplier = parseFloat(document.getElementById("7th_multiplier").value);
+    var compensation = parseFloat(document.getElementById("compensation").value);
+    var day_energy = t1_new - t1_old;
+    var night_energy = t2_new - t2_old;
+    var combination = day_energy + night_energy;
+    
+    var result = 13;
+    
+    result += day_energy * first_multiplier;
+    result += night_energy * second_multiplier;
+    result += combination * third_multiplier;
+    //result += combination * fourth_multiplier;
+    result += combination * fifth_multiplier;
+    result += combination * sixth_multiplier;
+    result += combination * seventh_multiplier;
+    result *= 1.2;
+    result -= combination * compensation;
+
+    console.log(combination);
+    document.getElementById("result").innerHTML = result.toFixed(2) + "лева."
+}
